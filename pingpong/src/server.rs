0c4c231c23f39174785b7a06157958d571904147
Cyril Plisko <v-cyril.plisko@wallaroo.ai>
2023-10-30 14:31:46 +0200

    server examples for single and multiple methods

--- pingpong/src/server.rs
+++ pingpong/src/server.rs
@@ -1,3 +1,5 @@
+use jsonrpc_nats::Server;
+
 use super::*;
 
 pub(super) async fn server(addrs: String) -> anyhow::Result<()> {
@@ -9,7 +11,37 @@ pub(super) async fn server(addrs: String) -> anyhow::Result<()> {
 
     tracing::info!(?server, "Starting");
 
-    // let ctx = pingpong::PingPong;
+    multiple(&server).await
+}
+
+async fn multiple(server: &Server) -> anyhow::Result<()> {
+    let pp = pingpong::PingPong;
+    let c = count::Count::default();
+    let s = simple::Simple;
+
+    let mut ep1 = server
+        .add_method::<pingpong::PingPong>()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+    let mut ep2 = server
+        .add_method::<count::Count>()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+    let mut ep3 = server
+        .add_method::<simple::Simple>()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    loop {
+        tokio::select! {
+            Some(_) = Server::serve_one(&mut ep1, &pp) => {},
+            Some(_) = Server::serve_one(&mut ep2, &c) => {},
+            Some(_) = Server::serve_one(&mut ep3, &s) => {},
+        }
+    }
+}
+
+async fn _single(server: &Server) -> anyhow::Result<()> {
     let ctx = count::Count::default();
 
     server
